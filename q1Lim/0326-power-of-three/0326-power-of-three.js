51998c57417779bc2be3f6ff84aced322b5d241c
q1Lim <cochlea00@gmail.com>
2025-11-23 17:11:17 +0900

    💡 [LTC] Math: 0326-power-of-three
    - Time: 6 ms (62.14%)
    - Space: 65.9 MB (27.43%)
    - Difficulty: Easy
    - Date: 11-23-2025

--- q1Lim/0326-power-of-three/0326-power-of-three.js
+++ q1Lim/0326-power-of-three/0326-power-of-three.js
@@ -3,6 +3,12 @@
  * @return {boolean}
  */
 var isPowerOfThree = function(n) {
+    /*
+        1. 3 이하의 숫자들은 false 처리
+        2. 3으로 나누었을 때 나머지가 0이 아닐때까지 계속 나눈다
+        3. n을 최대로 나누었을 때 n이 1이 아닌 경우 false, 나머지는 true
+    */
+    
     if (n <= 0) return false
 
     while(n % 3 === 0){
